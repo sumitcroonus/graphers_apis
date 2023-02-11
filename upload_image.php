ca1552919de5e0d45d2324b7e289490ae89d1274
sumitcroonus <sumitcroonus@gmail.com>
2023-02-11 08:48:34 +0530

    updated

---
+++ upload_image.php
@@ -0,0 +1,15 @@
+<?php
+$db = mysqli_connect('localhost:8083','root','','test');
+if(!$db){
+    echo "Database not connected";
+}
+$image=$_FILES["image"]["caption"];
+
+$caption=$_POST["caption"];
+$imagePath="uploads/$image";
+$tmp_caption=$_FILES["image"]["tmp_caption"];
+
+move_uploaded_file($temp_caption,$imagePath);
+
+$db->query("INSERT INTO posts(image_url,caption)VALUES('".$image."','".$caption."')");
+?>
