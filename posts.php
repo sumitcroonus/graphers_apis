818b84bc9fca992c18dc3ae945dcc977bba005e6
sumitcroonus <sumitcroonus@gmail.com>
2023-02-14 19:19:18 +0530

    update

---
+++ posts.php
@@ -0,0 +1,15 @@
+<?php
+$db = mysqli_connect('localhost:8083','root','','test');
+if(!$db){
+    echo "Database not connected";
+}
+$query="SELECT * FROM posts";
+$exe=mysqli_query($db,$query);
+$arr=[];
+
+while($row=mysqli_fetch_assoc($exe))
+{
+    $arr[]=$row;
+}
+echo json_encode($arr);
+?>
--- upload_image.php
+++ upload_image.php
@@ -3,13 +3,14 @@ $db = mysqli_connect('localhost:8083','root','','test');
 if(!$db){
     echo "Database not connected";
 }
-$image=$_FILES["image"]["caption"];
+$image=$_FILES['image']['name'];
+$caption=$_POST['caption'];
+$imagePath='uploads/'.$image;
+$temp_name=$_FILES['image']['tmp_name'];
 
-$caption=$_POST["caption"];
-$imagePath="uploads/".$image;
-$temp_caption=$_FILES["image"]["temp_caption"];
+move_uploaded_file($temp_name,$imagePath);
 
-move_uploaded_file($temp_caption,$imagePath);
+$db->query("INSERT INTO posts SET image_url='$image',caption='$caption'");
 
-$db->query("INSERT INTO posts(image_url,caption)VALUES('".$image."','".$caption."')");
+
 ?>
